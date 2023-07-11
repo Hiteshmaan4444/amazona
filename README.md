8bd953538f0db44b199a14f1dc39f62f76b73284
Hiteshmaan <Hiteshmaan472000@gmail.com>
2023-07-11 16:26:31 +0530

    Fetch Products from backend

--- README.md
+++ README.md
@@ -14,6 +14,7 @@
    3. create router for product screen
 
 7. Create Node Js Server
+
    1. run npm init in root folder
    2. Update package.json set type module
    3. Add .js to imports
@@ -26,3 +27,10 @@
    10. create route for /api/products
    11. return products
    12. run npm start
+
+8. Fetch Products from backend
+   1. install concurrently and setup
+   2. npm i axios
+   3. use state hook
+   4. use effect hook
+   5. use reducer hook
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import data from '../data';
+import React, { useEffect } from 'react';
+//import data from '../data';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import axios from 'axios';
+
 function HomeScreen() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('api/products');
+      setProducts(result.data);
+    };
+    fetchData();
+  }, []);
   return (
     <>
       <h1>Featured Products</h1>
       <div className="products">
-        {data.products.map((ele) => (
+        {products.map((ele) => (
           <div className="product" key={ele.slug}>
             <Link to={`/product/${ele.slug}`}>
               <img src={ele.images} alt={ele.name} />
